02be2451c1d08299496887f7db8cc1f1106e1590
Oskarovsky <oskar.slyk@gmail.com>
2021-09-05 14:26:54 +0200

    init

---
+++ src/test/java/com/oskarro/batchfiletransformer/BatchFileTransformerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oskarro.batchfiletransformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchFileTransformerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
